bf87bc7a6fd3ad10c28b993142889095d1c1e132
reneshibuya <carlosrenemail@gmail.com>
2021-06-01 22:21:55 +0200

    Preparing Component

---
+++ src/components/MH_Content/MH_Content.js
@@ -0,0 +1,11 @@
+import './MH_Content.css'
+
+const MH_Content = (props) => {
+    return (
+        <div>
+            {props.content}
+        </div>
+    )
+}
+
+export default MH_Content
---
+++ src/components/TestPage/TestPage.js
@@ -0,0 +1,12 @@
+import './TestPage.css'
+
+const TestPage = () => {
+    return (
+        <div className="TestPageMainDiv">
+        <h1>This is TestPage</h1>
+        </div>
+
+    )
+}
+
+export default TestPage
