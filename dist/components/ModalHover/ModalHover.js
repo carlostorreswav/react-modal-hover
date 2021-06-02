b0b82bd90dd51f16123ae28c67f05452e3ca7d48
reneshibuya <carlosrenemail@gmail.com>
2021-06-02 17:47:36 +0200

    Ok

--- dist/components/ModalHover/ModalHover.js
+++ dist/components/ModalHover/ModalHover.js
@@ -1,13 +1,19 @@
 "use strict";
 
+require("core-js/modules/web.dom-collections.iterator.js");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
 
-var _react = require("react");
+var _react = _interopRequireWildcard(require("react"));
 
 require("./ModalHover.css");
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
 
@@ -180,18 +186,18 @@ const ModelHover = props => {
     checkClose();
   };
 
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/_react.default.createElement("div", {
     className: "ModalHoverMainDiv",
     id: idMain.current = 'Main-' + Math.floor(Math.random() * 1000)
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/_react.default.createElement("div", {
     className: "ModalHoverBack",
     id: idBack.current = 'Back-' + Math.floor(Math.random() * 1000)
-  }), /*#__PURE__*/React.createElement("div", {
+  }), /*#__PURE__*/_react.default.createElement("div", {
     className: "ModalHoverChild",
     onMouseEnter: () => onMouseEnterChild(),
     onMouseLeave: () => onMouseLeaveChild(),
     id: idChild.current = 'Child-' + Math.floor(Math.random() * 1000)
-  }, props.children), /*#__PURE__*/React.createElement("div", {
+  }, props.children), /*#__PURE__*/_react.default.createElement("div", {
     className: "ModalHoverCont",
     id: idCont.current = 'Cont-' + Math.floor(Math.random() * 1000),
     onMouseEnter: () => onMouseEnterCont(),
